5b65c8642671ef1faa32ef44f0adb8dfea7648a4
Christian <91073858+HeyChriss@users.noreply.github.com>
2025-03-01 13:07:01 -0700

    Update server.py

--- server.py
+++ server.py
@@ -144,4 +144,4 @@ def get_active_thread():
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 8080))
-    application.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
